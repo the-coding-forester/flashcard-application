b4a42ed1bde97bc1ccb2cf58bc14a9723052e2e7
Stephanie Moore <smoore_forestry@outlook.com>
2021-05-30 15:24:22 -0400

    Fully functioning EditDeckPage

---
+++ src/Decks/EditDeckPage.js
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from "react"
+import { useHistory, useParams } from "react-router";
+import { updateDeck, readDeck } from "../utils/api";
+import DeckForm from "./DeckForm";
+
+function EditDeckPage() {
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const { deckId } = useParams();
+  const history = useHistory();
+
+  useEffect(() => {
+    (async () => {
+      const localDeck = await readDeck(deckId);
+      setName(localDeck.name);
+      setDescription(localDeck.description);
+    })()
+  }, [deckId])
+
+  const handleSubmit = async () => {
+    await updateDeck({ id: deckId, name, description })
+    setName("");
+    setDescription("")
+    history.push(`/decks/${deckId}`);
+  }
+
+  const handleCancel = () => {
+    setName("");
+    setDescription("")
+    history.push(`/decks/${deckId}`);
+  }
+
+  return (
+    <div class="container">
+      <h2>Edit Deck</h2>
+
+      <DeckForm
+        name={name}
+        description={description}
+        onNameChange={setName}
+        onDescriptionChange={setDescription}
+        onCancel={handleCancel}
+        onSubmit={handleSubmit}
+      />
+    </div >
+  )
+}
+
+export default EditDeckPage;
