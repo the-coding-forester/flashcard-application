4d0a01975563f718b99fb9811dfc63ac90e23d78
Stephanie Moore <smoore_forestry@outlook.com>
2021-05-31 12:04:13 -0400

    Minor Updates

--- src/Layout/Header.js
+++ src/Layout/Header.js
@@ -5,7 +5,7 @@ function Header() {
     <header className="jumbotron bg-dark">
       <div className="container text-white">
         <h1 className="display-4">Virtual Flashcards</h1>
-        <p className="lead">A better way to study and share resources</p>
+        <h2 className="lead">A better way to study and share resources</h2>
       </div>
     </header>
   );
